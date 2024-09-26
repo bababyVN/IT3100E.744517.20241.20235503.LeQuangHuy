accd8b66c919907c54582517aea6d1d559b75b5d
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-09-26 20:07:25 +0700

    Complete assignment 2.2.2

--- FirstDialog.java
+++ FirstDialog.java
@@ -3,6 +3,5 @@ public class FirstDialog {
     public static void main(String[] args){
         JOptionPane.showMessageDialog(null, "Hello world! How are you?");
         System.exit(0);
-        System.exit(0);
     }
 }
