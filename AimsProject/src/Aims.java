ed9ecd1869ec2ab3a4c21d3920e2e852e8f5b5cd
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-11-28 08:44:28 +0700

    Ex2 Lab03

--- AimsProject/src/Aims.java
+++ AimsProject/src/Aims.java
@@ -6,15 +6,19 @@ public class Aims {
 		Cart anOrder = new Cart();
 		
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-		anOrder.addDigitalVideoDisc(dvd1);
+		//anOrder.addDigitalVideoDisc(dvd1);
 
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-		anOrder.addDigitalVideoDisc(dvd2);
+		//anOrder.addDigitalVideoDisc(dvd2);
 		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 		anOrder.addDigitalVideoDisc(dvd3);
 		
+		DigitalVideoDisc[] dvd_list = {dvd2,dvd2,dvd2,dvd2,dvd2};
+		anOrder.addDigitalVideoDisc(dvd1,dvd1,dvd1);
 		//anOrder.removeDigitalVideoDisc(dvd2);
+		anOrder.addDigitalVideoDisc(dvd3,dvd3);
+		anOrder.addDigitalVideoDisc(dvd_list);
 		
 		System.out.println("Total cost is: " + anOrder.totalCost());
 		
