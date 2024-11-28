6b97eddccdc2569f096b8e8fee705570c35b14ec
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-11-28 16:26:16 +0700

    Ex 6 Lab 03

--- AimsProject/src/DigitalVideoDisc.java
+++ AimsProject/src/DigitalVideoDisc.java
@@ -1,11 +1,12 @@
 
 public class DigitalVideoDisc {
-	
+
 	private String title;
 	private String category;
 	private String director;
 	private int length;
 	private float cost;
+
 	
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -13,37 +14,44 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.length = length;
-		this.cost = cost;
+		
+
 	}
 	public DigitalVideoDisc(String director, String category, String title, float cost) {
 		super();
 		this.director = director;
 		this.category = category;
 		this.title = title;
-		this.cost = cost;
+		
+
 	}
 	public DigitalVideoDisc(String title, String catagory, float cost) {
 		super();
 		this.category = category;
 		this.title = title;
-		this.cost = cost;
+		
+
 	}
 	public DigitalVideoDisc(String title) {
 		super();
-		this.title = title;
-	}
+		
+
 	
 	public String getTitle() {
-		return title;
+		
+
 	}
 	public String getCategory() {
-		return category;
+		
+
 	}
 	public String getDirector() {
-		return director;
+		
+
 	}
 	public int getLength() {
-		return length;
+		
+
 	}
 	public float getCost() {
 		return cost;
