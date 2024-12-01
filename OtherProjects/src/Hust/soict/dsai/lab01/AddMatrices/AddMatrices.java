6bc3aaf92b0b428ff4ee7cd4c9e392198f95d966
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-12-01 16:52:05 +0700

    lab01 undo delete

--- OtherProjects/src/Hust/soict/dsai/lab01/AddMatrices/AddMatrices.java
+++ OtherProjects/src/Hust/soict/dsai/lab01/AddMatrices/AddMatrices.java
@@ -1,5 +1,44 @@
 package Hust.soict.dsai.lab01.AddMatrices;
 
+import java.util.Scanner;
 public class AddMatrices {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("Please enter the number of rows: ");
+        int rows = keyboard.nextInt();
+        System.out.println("Please enter the number of columns: ");
+        int cols = keyboard.nextInt();
+
+        int[][] matrixA = new int[rows][cols];
+        int[][] matrixB = new int[rows][cols];
+        System.out.println("The first matrix:");
+        inputMatrix(keyboard, matrixA, rows, cols);
+        System.out.println("The second matrix:");
+        inputMatrix(keyboard, matrixB, rows, cols);
+
+        int[][] sumMatrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sumMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+
+        System.out.println("Here's the sum of the two matrices:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sumMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    private static void inputMatrix(Scanner keyboard, int[][] matrix, int rows, int cols) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Please enter element ["+ (i+1) + "][" + (j+1) + "]: ");
+                matrix[i][j] = keyboard.nextInt();
+            }
+        }
+    }
 }
--- OtherProjects/src/Hust/soict/dsai/lab01/ArraySort/ArraySort.java
+++ OtherProjects/src/Hust/soict/dsai/lab01/ArraySort/ArraySort.java
@@ -1,5 +1,46 @@
 package Hust.soict.dsai.lab01.ArraySort;
 
+import java.util.Scanner;
 public class ArraySort {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Please enter the number of elements in the array: ");
+        int n = keyboard.nextInt();
+        int[] array = new int[n];
 
+        System.out.println("Please enter each of the " + n + " elements one by one:");
+        for (int i = 0; i < n; i++) {
+            array[i] = keyboard.nextInt();
+        }
+
+        Sort(array, n);
+
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        int average = sum / n;
+
+        String sortedarray = "";
+        for (int num : array) {
+            sortedarray += num + " ";
+        }
+        System.out.println("Here's the result:");
+        System.out.println(" Sorted Array: " + sortedarray);
+        System.out.println(" Sum: " + sum);
+        System.out.println(" Average: " + average);
+    }
+    
+    private static void Sort(int[] array, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
