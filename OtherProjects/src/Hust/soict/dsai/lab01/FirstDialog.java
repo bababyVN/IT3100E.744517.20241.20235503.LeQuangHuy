3dc90883f9ed5777fe3a8e84d0e3b9cf9ccc88fc
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-12-03 00:58:40 +0700

    Ex01 Lab4

---
+++ OtherProjects/src/Hust/soict/dsai/lab01/FirstDialog.java
@@ -0,0 +1,8 @@
+package Hust.soict.dsai.lab01;
+import javax.swing.JOptionPane;
+public class FirstDialog {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
