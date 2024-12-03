e413028ad0509609e881db9021431dff51d6dcba
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-12-04 02:35:43 +0700

    Ex 5 Lab04

--- AimsProject/src/hust/soict/dsai/aims/media/Book.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -5,10 +5,21 @@ import java.util.List;
 
 public class Book extends Media {
     private List<String> authors = new ArrayList<String>();
-
-    public Book() {
-
+    
+    public Book(String title) {
+        super(title);
     }
+    public Book(String title, String category) {
+        super(title, category);
+    }
+    public Book(String title, String category, float cost) {
+        super(title, category, cost);
+    }
+    public Book(String title, String category, float cost, List<String> authors) {
+        super(title, category, cost);
+        this.authors = authors;
+    }
+
     public void addAuthor(String authorName) {
     	if (!authors.contains(authorName)) {
     		authors.add(authorName);
@@ -19,7 +30,7 @@ public class Book extends Media {
     	}
     }
     public void removeAuthor(String authorName) {
-    	if (authors.contains(authorName)) { // Check if the author is present in the list
+    	if (authors.contains(authorName)) {
             authors.remove(authorName);
             System.out.println("Author removed: " + authorName);
         } else {
