0da0d5a49006e0a5995a6e4a5e42740d7f29c356
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-12-04 00:56:48 +0700

    Ex 2 Lab 04

--- AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -1,38 +1,50 @@
 package hust.soict.dsai.aims;
+
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 
 public class Aims {
 
 	public static void main(String[] args) {
-		
+
 		Cart anOrder = new Cart();
-		
+
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-		//anOrder.addDigitalVideoDisc(dvd1);
+		// anOrder.addDigitalVideoDisc(dvd1);
 
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-		//anOrder.addDigitalVideoDisc(dvd2);
-		
+		// anOrder.addDigitalVideoDisc(dvd2);
+
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 		DigitalVideoDisc dvd4 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
 		anOrder.addDigitalVideoDisc(dvd3);
-		
-		DigitalVideoDisc[] dvd_list = {dvd2,dvd2,dvd2,dvd2,dvd2};
-		anOrder.addDigitalVideoDisc(dvd1,dvd1,dvd1);
-		//anOrder.removeDigitalVideoDisc(dvd2);
-		anOrder.addDigitalVideoDisc(dvd3,dvd4);
+
+
+		DigitalVideoDisc[] dvd_list = { dvd2, dvd2, dvd2, dvd2, dvd2 };
+		anOrder.addDigitalVideoDisc(dvd1, dvd1, dvd4);
+		// anOrder.removeDigitalVideoDisc(dvd2);
+		anOrder.addDigitalVideoDisc(dvd3, dvd1);
 		anOrder.addDigitalVideoDisc(dvd_list);
-		
+
+		// DigitalVideoDisc[] dvd_list = {dvd2,dvd2,dvd2,dvd2,dvd2};
+		anOrder.addDigitalVideoDisc(dvd1, dvd1, dvd2);
+		// anOrder.removeDigitalVideoDisc(dvd2);
+		anOrder.addDigitalVideoDisc(dvd3, dvd4);
+		// anOrder.addDigitalVideoDisc(dvd_list);
+
+
 		System.out.println(dvd1.getId());
 		System.out.println("Total cost is: " + anOrder.totalCost());
 		anOrder.print();
-		
-        anOrder.searchById(2);	
 
-        // Search by title
-        anOrder.searchByTitle("Aladin");
-    }
 
+		anOrder.searchById(2);
+
+		anOrder.searchById(3);
+
+
+		// Search by title
+		anOrder.searchByTitle("Aladin");
+	}
 
 }
--- AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
+++ AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
@@ -4,7 +4,6 @@ import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 public class TestPassingParameter {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
