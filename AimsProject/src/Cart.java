6e4dc2dd3a5cfce2cbf25263032903e33b6727c3
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-12-04 02:09:11 +0700

    Ex 5 Lab04

---
+++ AimsProject/src/Cart.java
@@ -0,0 +1,144 @@
+
+package hust.soict.dsai.aims.cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+
+public class Cart {
+
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = 
+			new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private int qtyOrdered = 0 ;
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+			if (itemsOrdered[i] == disc) {
+				itemsOrdered[i] = null;
+				qtyOrdered -= 1;
+				System.out.println("The disc has been removed: " + disc.getTitle());
+				break;
+			}
+		}
+	}
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+			System.out.println("The cart is full");
+		}else {
+			if (qtyOrdered == MAX_NUMBERS_ORDERED - 1) {
+				System.out.println("The cart is almost full");
+			}
+			for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+				if (itemsOrdered[i] == null) {
+					itemsOrdered[i] = disc;
+					qtyOrdered += 1;
+					System.out.println("The disc has been added: " + disc.getTitle());
+					break;
+				}
+			}
+		}
+	}
+	//public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2) {
+		//addDigitalVideoDisc(dvd1);
+		//addDigitalVideoDisc(dvd2);
+	//}
+	//public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+	//	for (DigitalVideoDisc disc : dvdList) {
+	//		if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+	//			System.out.println("The cart is full");
+	//		}else {
+	//			if (qtyOrdered == MAX_NUMBERS_ORDERED - 1) {
+	//				System.out.println("The cart is almost full");
+	//			}
+	//			for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+	//				if (itemsOrdered[i] == null) {
+	//					itemsOrdered[i] = disc;
+	//					qtyOrdered += 1;
+	//					System.out.println("The disc has been added: " + disc.getTitle());
+	//					break;
+	//				}
+	//			}
+	//		}
+    //    }	
+	//}
+	public void addDigitalVideoDisc(DigitalVideoDisc... dvds) {
+		for (DigitalVideoDisc disc : dvds) {
+			if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+				System.out.println("The cart is full");
+			}else {
+				if (qtyOrdered == MAX_NUMBERS_ORDERED - 1) {
+					System.out.println("The cart is almost full");
+				}
+				for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+					if (itemsOrdered[i] == null) {
+						itemsOrdered[i] = disc;
+						qtyOrdered += 1;
+						System.out.println("The disc has been added: " + disc.getTitle());
+						break;
+					}
+				}
+			}
+        }	
+	}
+	public float totalCost()  {
+		float total = 0;
+	    for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+	        if (itemsOrdered[i] != null) {
+	            total += itemsOrdered[i].getCost();
+	        }
+	    }
+	    return total;
+	}
+	 public void print() {
+	        System.out.println("***********************CART***********************");
+	        System.out.println("Ordered Items:");
+	        float totalCost = 0;
+
+	        for (int i = 0; i < itemsOrdered.length; i++) {
+	        	if (itemsOrdered[i] != null) {
+	            	DigitalVideoDisc dvd = itemsOrdered[i];
+	            	System.out.println((i + 1) + ". " + dvd.toString());
+	            	totalCost += dvd.getCost();
+	        	}
+	        }
+
+	        System.out.println("Total cost: " + totalCost + " $");
+	        System.out.println("***************************************************");
+	    }	
+	 public void searchById(int id) {
+	        boolean found = false;
+	        for (DigitalVideoDisc dvd : itemsOrdered) {
+	        	if (dvd != null) {
+	        		int ID = dvd.getId();
+	            	if (ID == id) {
+	            		if (!found) {
+	            			System.out.println("\nSearch by ID (ID: '"+ id +"'):");
+	            		}
+	            		System.out.println(dvd.toString());
+	            		found = true;
+	            	}
+	            }
+	        }
+
+	        if (!found) {
+	            System.out.println("No DVD found with ID: " + id);
+	        }
+	    }
+	 public void searchByTitle(String title) {
+	        boolean found = false;
+	        for (DigitalVideoDisc dvd : itemsOrdered) {
+	        	if (dvd != null) {
+	            	if (dvd.isMatch(title)) {
+	            		if (!found) {
+	            			System.out.println("\nSearch by Title (Keyword: '"+ title +"'):");
+	            		}
+	            		System.out.println(dvd.toString());
+	            		found = true;
+	            	}
+	            }
+	        }
+
+	        if (!found) {
+	            System.out.println("No DVD found with title containing: " + title);
+	        }
+	    }	
+}
---
+++ AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,96 @@
+package hust.soict.dsai.aims.disc;
+
+public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+
+	
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
+	
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
+	
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
+	
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+		nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+	}
+	public DigitalVideoDisc(String director, String category, String title, float cost) {
+		super();
+		this.director = director;
+		this.category = category;
+		this.title = title;
+		this.cost = cost;
+		nbDigitalVideoDiscs++;
+    this.id = nbDigitalVideoDiscs;
+	}
+	public DigitalVideoDisc(String title, String catagory, float cost) {
+		super();
+		this.category = category;
+		this.title = title;
+		
+
+	}
+	public DigitalVideoDisc(String title) {
+		super();
+		
+
+		this.cost = cost;
+		nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+	}
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+		nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+	}
+	
+	public String getTitle() {
+		
+
+	}
+	public String getCategory() {
+		
+
+	}
+	public String getDirector() {
+		
+
+	}
+	public int getLength() {
+		
+
+	}
+	public float getCost() {
+		return cost;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getId() {
+        return id;
+    }
+	public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
+	public String toString() {
+        return "DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
+    }
+	public boolean isMatch(String title) {
+        if (this.title == null) return false;
+        return this.title.equalsIgnoreCase(title) || this.title.toLowerCase().contains(title.toLowerCase());
+    }
+}
