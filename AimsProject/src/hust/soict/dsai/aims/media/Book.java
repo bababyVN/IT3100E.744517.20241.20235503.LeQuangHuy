80624d17fd9f968013dfab52d6cd525116543eeb
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-12-04 02:00:34 +0700

    Ex 5 Lab 04

--- AimsProject/src/hust/soict/dsai/aims/media/Book.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -1,9 +1,40 @@
 package hust.soict.dsai.aims.media;
 
-public class Book {
+import java.util.ArrayList;
+import java.util.List;
 
-	public Book() {
-		// TODO Auto-generated constructor stub
-	}
+public class Book extends Media {
+    private List<String> authors = new ArrayList<String>();
+    
+    public Book(String title) {
+        super(title);
+    }
+    public Book(String title, String category) {
+        super(title, category);
+    }
+    public Book(String title, String category, float cost) {
+        super(title, category, cost);
+    }
+    public Book(String title, String category, float cost, List<String> authors) {
+        super(title, category, cost);
+        this.authors = authors;
+    }
 
+    public void addAuthor(String authorName) {
+    	if (!authors.contains(authorName)) {
+    		authors.add(authorName);
+    		System.out.println("Author added: " + authorName);
+    	}
+    	else {
+    		System.out.println("Author already exists: " + authorName);
+    	}
+    }
+    public void removeAuthor(String authorName) {
+    	if (authors.contains(authorName)) { // Check if the author is present in the list
+            authors.remove(authorName);
+            System.out.println("Author removed: " + authorName);
+        } else {
+            System.out.println("Author not found: " + authorName);
+        }
+    }
 }
---
+++ AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -0,0 +1,64 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.List;
+
+public class CompactDisc extends Media {
+	private String artist;
+    private List<Track> tracks;
+    
+    public CompactDisc(String title) {
+        super(title);
+    }
+
+    public CompactDisc(String title, String category) {
+        super(title, category);
+    }
+
+    public CompactDisc(String title, String category, float cost) {
+        super(title, category, cost);
+    }
+
+    public CompactDisc(int id, String title, String category, float cost) {
+        super(id, title, category, cost);
+    }
+    public CompactDisc(int id, String title, String category, float cost, String artist) {
+        super(id, title, category, cost);
+        this.artist = artist;
+    }
+    public CompactDisc(int id, String title, String category, float cost, String artist, List<Track> tracks) {
+        super(id, title, category, cost);
+        this.artist = artist;
+        this.tracks = tracks;
+    }
+    public CompactDisc(String title, String category, float cost, String artist, List<Track> tracks) {
+        super(title, category, cost);
+        this.artist = artist;
+        this.tracks = tracks;
+    }
+    public CompactDisc(String title, String category, float cost, String artist) {
+        super(title, category, cost);
+        this.artist = artist;
+    }
+    
+    public void addTrack(Track track) {
+    	if (!tracks.contains(track)) {
+    		tracks.add(track);
+    		System.out.println("Track added: " + track);
+    	}
+    	else {
+    		System.out.println("Track already exists: " + track);
+    	}
+    }
+    public void removeTrack(Track track) {
+    	if (tracks.contains(track)) { // Check if the author is present in the list
+    		tracks.remove(track);
+            System.out.println("Track removed: " + track);
+        } else {
+            System.out.println("Track not found: " + track);
+        }
+    }
+    
+    public String getArtist() {
+        return artist;
+    }
+}
