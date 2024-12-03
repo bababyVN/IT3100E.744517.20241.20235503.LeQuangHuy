3ff3c825c9fb62615fb6cfb1b58de23b0b3523c6
bababyVN <87956165+bababyVN@users.noreply.github.com>
2024-12-04 01:33:51 +0700

    Ex 4 Lab 04

--- AimsProject/src/hust/soict/dsai/aims/media/Book.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -1,9 +1,29 @@
 package hust.soict.dsai.aims.media;
 
-public class Book {
+import java.util.ArrayList;
+import java.util.List;
 
-	public Book() {
-		// TODO Auto-generated constructor stub
-	}
+public class Book extends Media {
+    private List<String> authors = new ArrayList<String>();
 
+    public Book() {
+
+    }
+    public void addAuthor(String authorName) {
+    	if (!authors.contains(authorName)) {
+    		authors.add(authorName);
+    		System.out.println("Author added: " + authorName);
+    	}
+    	else {
+    		System.out.println("Author already exists: " + authorName);
+    	}
+    }
+    public void removeAuthor(String authorName) {
+    	if (authors.contains(authorName)) { // Check if the author is present in the list
+            authors.remove(authorName);
+            System.out.println("Author removed: " + authorName);
+        } else {
+            System.out.println("Author not found: " + authorName);
+        }
+    }
 }
